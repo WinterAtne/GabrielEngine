a188f482d5a091dd9ad20cccb2b97485120b87da
WinterAtne <104174438+WinterAtne@users.noreply.github.com>
2025-06-08 18:55:06 -0600

    Index Array Hello Triangle

--- src/main.c
+++ src/main.c
@@ -42,12 +42,15 @@ int main() {
 
 	// Setup Triangle
 	GLfloat vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		-0.5,   0.5f, 0.0f,
-		-0.5f,  0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.5,   0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f, // Bottom Left
+		 0.5f, -0.5f, 0.0f, // Bottom Right
+		-0.5f,  0.5f, 0.0f, // Top Left
+		 0.5,   0.5f, 0.0f, // Top Right
+	};
+
+	GLuint indices[] = {
+		0, 1, 2,
+		2, 1, 3,
 	};
 
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -66,20 +69,26 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	GLuint VAO, VBO;
+	GLuint VAO, VBO, EBO;
 
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	
 	// Main Loop
 	while(!glfwWindowShouldClose(window)) {
@@ -89,7 +98,7 @@ int main() {
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 	}
@@ -97,6 +106,7 @@ int main() {
 	// Tear Down
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 	glfwDestroyWindow(window);
 	glfwTerminate();
