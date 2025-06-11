b4191d18352db0f77b15bad1fc3abdc69b8dbed0
WinterAtne <104174438+WinterAtne@users.noreply.github.com>
2025-06-11 17:41:56 -0600

    Removed unnessesary vertex attribute

--- src/main.c
+++ src/main.c
@@ -67,9 +67,9 @@ int main() {
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
-	// glBindBuffer(GL_ARRAY_BUFFER, 0);
-	// glBindVertexArray(0);
-	// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 
 	// Model
