0d6ea3fa81bf131f9dc68075bc6886c5afb9712c
WinterAtne <104174438+WinterAtne@users.noreply.github.com>
2025-06-26 03:14:23 -0600

    Massive code changed and Major bug fixes: moved engine into rendering and fixed pointer mistakes with sprites

--- src/main.c
+++ src/main.c
@@ -1,28 +1,40 @@
-#include "engine.h"
 #include "rendering.h"
 
+#include <cglm/io.h>
+
 int main() {
-	engine_init(800, 600, 1, "Hello World!", (float[4]){0.1f, 0.1f, 0.1f, 1.0f});
+	rendering_initialize(800, 600, 1, "Hello World!", (float[4]){0.1f, 0.1f, 0.1f, 1.0f});
 
 	Texture tex0 = texture_make("resources/textures/test_text.png");
-	Sprite* s0 = sprite_make();
+	SpriteID s0 = sprite_make();
+	sprite_transform_translate(s0, (vec3){-0.5f, -0.0f, 0.0f});
 	sprite_texture_set(s0, tex0);
 
-	Sprite* s1 = sprite_make();
+	SpriteID s1 = sprite_make();
 	sprite_transform_translate(s1, (vec3){-1.2f, 0.5f, 0.0f});
 	Texture tex1 = texture_make("resources/textures/test_text_1.jpg");
 	sprite_texture_set(s1, tex1);
 
 	// Main Loop
-	while(!engine_should_close()) {
-		sprite_transform_scale(s0, (vec3){0.999f, 1.01f, 1.0f});
+	char i = 0;
+	int prevID = 2;
+	while(!window_should_close()) {
+		sprite_transform_scale(s0, (vec3){0.99999f, 1.0001f, 1.0f});
 		sprite_transform_rotate(s0, 0.001f);
-		sprite_transform_translate(s0, (vec3){0.001f, 0.001f, 0.0f});
+		sprite_transform_translate(s0, (vec3){0.0001f, -0.0001f, 0.0f});
+		// glm_mat4_print(s0->transform, stdout);
 		// camera_transform_translate((vec3){0.022f, 0.01f, 0.0});
-		engine_process();
+
+		SpriteID s2 = sprite_make();
+		sprite_transform_translate(s2, (vec3){0.01f*i, 0.012f*i, 0.0f});
+		sprite_transform_rotate(s2, 0.01f*i);
+		sprite_texture_set(s2, tex1);
+
+		i++;
+		rendering_process();
 	}
 
 	// Tear Down
-	engine_end();
+	window_close();
 	return 0;
 }
--- src/rendering.h
+++ src/rendering.h
@@ -6,26 +6,30 @@ typedef struct {
 	GLuint handle;
 } Texture;
 
-typedef struct {
-	int ID;
-	mat4 transform;
-	Texture texture;
-} Sprite;
+typedef int SpriteID;
 
 // Initializing and Processing
-void sprites_initialize(int window_x, int window_y, float window_scale);
-void sprites_draw(); // Draws the sprite queue
+int rendering_initialize(
+		int window_x,
+		int window_y,
+		float window_scale,
+		const char* window_name,
+		const float clear_color[4]);
+
+void rendering_process();
+int window_should_close();
+void window_close();
 
 // Creating
 Texture texture_make(const char* texture_location);
-Sprite* sprite_make(); // Makes sprite, and adds it to the queue
-void sprite_free(Sprite* sprite); // Frees sprite, and removes it from queue
+SpriteID sprite_make(); // Makes sprite, and adds it to the queue
+void sprite_free(SpriteID sprite); // Frees sprite, and removes it from queue
 
 // Camera
 void camera_transform_translate(vec3 translation);
 
 // Modifying
-void sprite_texture_set(Sprite* sprite, Texture texture);
-void sprite_transform_translate(Sprite* sprite, vec3 translation);
-void sprite_transform_rotate(Sprite* sprite, float rotation);
-void sprite_transform_scale(Sprite* sprite, vec3 scale);
+void sprite_texture_set(SpriteID sprite, Texture texture);
+void sprite_transform_translate(SpriteID sprite, vec3 translation);
+void sprite_transform_rotate(SpriteID sprite, float rotation);
+void sprite_transform_scale(SpriteID sprite, vec3 scale);
