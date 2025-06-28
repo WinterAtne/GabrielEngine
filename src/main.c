e8693b3a1077a1b2fae0f2dbb37819ef8e635e9a
WinterAtne <104174438+WinterAtne@users.noreply.github.com>
2025-06-28 17:58:09 -0600

    Added layers for sprite rendering

--- src/main.c
+++ src/main.c
@@ -7,27 +7,27 @@ int main() {
 
 	Texture tex0 = texture_make("resources/textures/test_text.png");
 	Sprite s0 = sprite_make();
-	sprite_transform_translate(s0, (vec3){-0.5f, -0.0f, 0.0f});
+	sprite_translate(s0, (vec3){-0.5f, -0.0f, 1023.0f});
 	sprite_texture_set(s0, tex0);
 
 	Sprite s1 = sprite_make();
-	sprite_transform_translate(s1, (vec3){-1.2f, 0.5f, 0.0f});
+	sprite_translate(s1, (vec3){-1.2f, 0.5f, 0.0f});
 	Texture tex1 = texture_make("resources/textures/test_text_1.jpg");
 	sprite_texture_set(s1, tex1);
 
 	// Main Loop
 	char i = 0;
 	while(!window_should_close()) {
-		sprite_transform_scale(s0, (vec3){0.999f, 1.0011f, 1.0f});
-		sprite_transform_rotate(s0, 0.001f);
-		sprite_transform_translate(s0, (vec3){0.0001f, -0.0001f, 0.0f});
+		sprite_scale(s0, (vec3){0.999f, 1.0011f, 1.0f});
+		sprite_rotate(s0, 0.001f);
+		sprite_translate(s0, (vec3){0.0001f, -0.0001f, 0.0f});
 		// glm_mat4_print(s0->transform, stdout);
 		// camera_transform_translate((vec3){0.022f, 0.01f, 0.0});
 
 		Sprite s2 = sprite_make();
-		sprite_transform_translate(s2, (vec3){0.01f*i, 0.0002f*i*i, 0.0f});
-		sprite_transform_rotate(s2, 0.01f*i);
-		sprite_transform_scale(s2, (vec3){1.0f + 0.0001f*i*i, 1.0f, 1.0f});
+		sprite_translate(s2, (vec3){0.01f*i, 0.0002f*i*i, (float)i});
+		sprite_rotate(s2, 0.01f*i);
+		sprite_scale(s2, (vec3){1.0f + 0.0001f*i*i, 1.0f, 1.0f});
 		sprite_texture_set(s2, tex1);
 
 		camera_transform_translate((vec3){-0.001f, 0.001f, 0.0f});
