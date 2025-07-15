65404aea45744da98285c0201a8c3d05f300293f
WinterAtne <104174438+WinterAtne@users.noreply.github.com>
2025-07-14 20:35:35 -0600

    Recursive scene parsing

--- main.go
+++ main.go
@@ -9,46 +9,15 @@ import (
 func main() {
 	core.Start()
 
-	scene := tree.GetScene("example.json")
+	scene := tree.GetScene("main.json")
 	node_0 := scene.Instantiate()
 
-	// tex := core.GetTexture("test.png")
-	// sha := core.GetShader("default")
-	// node_1 := tree.NewNode(&tree.Sprite{Texture: &tex, Shader: &sha}, "node_1")
-	// node_1.Transform.PositionX += 64
-
 	tree.GetRoot().AddChild(node_0)
-	// tree.GetRoot().AddChild(node_1)
-
-	// tex := core.GetTexture("test.png")
-	// core.FinishFrame()
-	// core.BlockTillNextFrame()	
-	// 
-	// // for i := range 16 {
-	// // 	n := tree.NewNode(&tree.Sprite{Texture: &tex}, "sprite" + strconv.Itoa(i))
-	// // 	tree.GetRoot().AddChild(n)
-	// // 	n.Transform.PositionY = float32(i*16) + float32(- 128 + 8)
-	// // }
-	// //
-	// // for i := range 16 {
-	// // 	n := tree.NewNode(&tree.Sprite{Texture: &tex}, "sprite" + strconv.Itoa(i))
-	// // 	tree.GetRoot().AddChild(n)
-	// // 	n.Transform.PositionX = float32(i*16) + float32(- 128 + 8)
-	// // }
-	//
-	// character := tree.NewNode(&tree.Sprite{Texture: &tex, Shader: core.GetShader("circle")}, "character")
-	// tree.GetRoot().AddChild(character)
-	//
 
 	tree.Process(0)
-	// fmt.Println(node_0.GetChildren())
 
 	finish := true
 	for finish {
-
-		// fmt.Println(*node_0)
-		// fmt.Println(*node_1)
-		// tree.GetRoot().Transform.Rotation += 0.016
 
 		tree.Process(0)
 
--- tree/scene.go
+++ tree/scene.go
@@ -12,7 +12,7 @@ type Scene struct {
 
 var scenes map[string]*Scene = make(map[string]*Scene)
 
-var typeToScript map[string](func() Script) = map[string]func() Script{
+var typeToScript map[string](func() Script) = map[string]func() Script {
 	"Sprite": func() Script {return new(Sprite)},
 	"Object": func() Script {return new(Object)},
 }
@@ -63,32 +63,42 @@ func (node *Node) UnmarshalJSON(data []byte) error {
 	err := json.Unmarshal(data, &defs)
 	if err != nil { panic(err) }
 
-	script := typeToScript[string(defs["type"])[1:len(defs["type"])-1]]()
-	name := string(defs["name"])[1:len(defs["name"])-1]
-	newNode := NewNode(script, name)
+	if t, has := defs["type"]; has {
+		script := typeToScript[string(t)[1:len(defs["type"])-1]]()
+		name := string(defs["name"])[1:len(defs["name"])-1]
+		newNode := NewNode(script, name)
 
-	*node = *newNode
-	if trans, has := defs["transform"]; has {
-		err := json.Unmarshal(trans, &node.Transform)
+		*node = *newNode
+		if trans, has := defs["transform"]; has {
+			err := json.Unmarshal(trans, &node.Transform)
+			if err != nil { panic(err) }
+		} else {
+			node.Transform = newNode.Transform
+		}
+		
+		err = json.Unmarshal(data, &node.Script)
 		if err != nil { panic(err) }
-	} else {
-		node.Transform = newNode.Transform
-	}
-
-	err = json.Unmarshal(data, &node.Script)
-	if err != nil { panic(err) }
-
-	if c, has := defs["children"]; has {
-		var rawChildren []json.RawMessage
-		err = json.Unmarshal(c, &rawChildren)
-		if err != nil { panic(err) }
-		for _, childData := range rawChildren {
-			var child *Node
-			err := json.Unmarshal(childData, &child)
-			if err != nil {panic(err)}
-			node.AddChild(child)
+		
+		if c, has := defs["children"]; has {
+			var rawChildren []json.RawMessage
+			err = json.Unmarshal(c, &rawChildren)
+			if err != nil { panic(err) }
+			for _, childData := range rawChildren {
+				var child *Node
+				err := json.Unmarshal(childData, &child)
+				if err != nil {panic(err)}
+				node.AddChild(child)
+			}
+		}
+	} else if sstr, has := defs["scene"]; has {
+		if sdef, has := scenes[string(sstr)[1:len(sstr)-1]]; has {
+			err := json.Unmarshal([]byte(sdef.definition), node)
+			if err != nil {
+				panic(err)
+			}
 		}
 	}
+
 
 	return nil
 }
